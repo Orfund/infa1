2dba86e924a19a7896d250a2f353646a9a61c1e9
Orfund <cendleader@gmail.com>
2018-12-12 01:08:07 +0300

    Add files via upload

---
+++ list.h
@@ -0,0 +1,85 @@
+//
+//  list.h
+//  somefuck
+//
+//  Created by Дмитрий Маслюков on 11/12/2018.
+//  Copyright © 2018 Дмитрий Маслюков. All rights reserved.
+//
+
+#ifndef list_h
+#define list_h
+#include<stdlib.h>
+enum order {ASC=1, DESC=-1};
+template <typename obj>
+class List{
+private:
+    obj*Objects;
+    
+public:
+    typedef int(*ordrel)(obj,obj);
+    typedef int(*eqrel)(obj,obj);
+    int length;
+    List(int n = 1){
+        length = n;
+        Objects = (obj*)malloc(n*sizeof(obj));
+    }
+    ~List(){
+        free(Objects);
+    }
+    
+    List &operator=(List &l){
+        Objects = (obj*)realloc(Objects,l.length*sizeof(obj));
+        length = l.length;
+        for(int i = 0; i<length; i++)
+            Objects[i] = l[i];
+        return *this;
+    }
+    
+    obj &operator[] (int n) {
+        if(n<length)
+            return Objects[n];
+        else
+            throw("Index out of bounds");
+    }
+    int Find(obj O, eqrel F=[](obj a, obj b)->int{return a ==b;}){
+        for(int i = 0;i<length;i++)
+            if(F(Objects[i],O)==1)
+                return i;
+        return -1;
+    }
+    
+    void Append(obj O){
+        Objects = (obj*)realloc(Objects,(length+1)*sizeof(obj));
+        Objects[length] = O;
+        length++;
+    }
+    
+    void Sort(ordrel f){
+        int j,k,i;
+        obj buf;
+        for(k = length/2; k > 0; k /=2)
+            for(i = k; i < length; i++)
+            {
+                buf = Objects[i];
+                for(j = i; j>=k; j-=k)
+                {
+                    if(f(buf,Objects[j-k])==0)
+                        Objects[j] = Objects[j-k];
+                    else
+                        break;
+                }
+                Objects[j] = buf;
+            }
+        
+
+    }
+    
+   
+    
+    
+};
+
+
+
+
+#endif /* list_h */
