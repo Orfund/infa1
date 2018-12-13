d861c0f27e0865e8ba4497d213a60e22aa66f860
Orfund <cendleader@gmail.com>
2018-12-13 17:30:08 +0300

    Add files via upload

--- list.h
+++ list.h
@@ -14,13 +14,23 @@ template <typename obj>
 class List{
 private:
     obj*Objects;
-    
+    int __mem_res;
+    int length;
+    void Rloc(){
+        Objects = (obj*)realloc(Objects,(__mem_res)*sizeof(obj));
+    }
 public:
     typedef int(*ordrel)(obj,obj);
     typedef int(*eqrel)(obj,obj);
-    int length;
+    typedef int(*sigma)(int);
+    
+    int Length(){
+        return length;
+    }
+    
     List(int n = 1){
-        length = n;
+        length = 0;
+        __mem_res = n;
         Objects = (obj*)malloc(n*sizeof(obj));
     }
     ~List(){
@@ -28,7 +38,8 @@ public:
     }
     
     List &operator=(List &l){
-        Objects = (obj*)realloc(Objects,l.length*sizeof(obj));
+        __mem_res = l.length;
+        Rloc();
         length = l.length;
         for(int i = 0; i<length; i++)
             Objects[i] = l[i];
@@ -36,11 +47,14 @@ public:
     }
     
     obj &operator[] (int n) {
+        if (n<0)
+            n = length -n;
         if(n<length)
             return Objects[n];
         else
             throw("Index out of bounds");
     }
+    
     int Find(obj O, eqrel F=[](obj a, obj b)->int{return a ==b;}){
         for(int i = 0;i<length;i++)
             if(F(Objects[i],O)==1)
@@ -48,11 +62,46 @@ public:
         return -1;
     }
     
+    List Reshuffle(sigma s){ //sigma must be biective
+        List<obj> list = List<obj>(length);
+        list = *this;
+        for(int i = 0; i<length; i++)
+            list[s(i)] = Objects[i];
+        return list;
+    }
+    
     void Append(obj O){
-        Objects = (obj*)realloc(Objects,(length+1)*sizeof(obj));
+        if(length==__mem_res){
+             __mem_res<<=1;
+            Rloc();
+           
+        }
         Objects[length] = O;
         length++;
     }
+    
+    void Remove(int i){
+        if (i<0 || i >=length)
+            throw "Out of bounds";
+        else{
+            for(int j = i+1;j<length;j++)
+                Objects[j-1] = Objects[j];
+            length--;
+            if(length<=__mem_res>>1){
+                __mem_res >>=1;
+                Rloc();
+                
+            }
+        }
+           
+    }
+    
+    void _memory(){
+        __mem_res = length;
+        Rloc();
+        
+    }
+    
     
     void Sort(ordrel f){
         int j,k,i;
